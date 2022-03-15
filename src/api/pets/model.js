90252f1192371f0b210a2f8d5b4697861cf2b9fd
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2022-03-14 21:36:49 -0600

    Remove dead code

--- src/api/pets/model.js
+++ src/api/pets/model.js
@@ -1,6 +1,4 @@
 const genModel = require("../../utils/genModel");
-
-const name = "Pets";
 const tableName = "pets";
 
 // Properties that are allowed to be selected from the database for reading.
@@ -9,7 +7,6 @@ const selectableProps = ["id", "name", "pet_types_id", "food_types_id"];
 module.exports = knex => {
 	const model = genModel({
 		knex,
-		name,
 		tableName,
 		selectableProps
 	});
