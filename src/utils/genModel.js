7ca16fc6e79ee6747d47eeb0f50712313e3e7193
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2022-03-14 21:36:34 -0600

    Refactor

--- src/utils/genModel.js
+++ src/utils/genModel.js
@@ -7,71 +7,69 @@
 // needed by composing a new object out of the one returned by this function ;)
 module.exports = ({
 	knex = {},
-	name = "name",
 	tableName = "tablename",
 	selectableProps = [],
 	timeout = 1000
 }) => {
-	const create = props => {
+	const create = (props, displayProps = selectableProps) => {
 		delete props.id; // not allowed to set `id`
 
 		return knex
 			.insert(props)
-			.returning(selectableProps)
+			.returning(displayProps)
 			.into(tableName)
 			.timeout(timeout);
 	};
 
-	const findAll = () =>
-		knex.select(selectableProps).from(tableName).timeout(timeout);
-
-	const find = filters =>
-		knex
-			.select(selectableProps)
-			.from(tableName)
-			.where(filters)
-			.timeout(timeout);
-
-	// Same as `find` but only returns the first match if >1 are found.
-	const findOne = filters =>
-		find(filters).then(results => {
-			if (!Array.isArray(results)) return results;
-
-			return results[0];
-		});
-
-	const findById = id =>
-		knex
-			.select(selectableProps)
-			.from(tableName)
-			.where({ id })
-			.timeout(timeout);
-
-	const update = (id, props) => {
+	const update = (id, props, displayProps = selectableProps) => {
 		delete props.id; // not allowed to set `id`
 
 		return knex
 			.update(props)
 			.from(tableName)
 			.where({ id })
-			.returning(selectableProps)
+			.returning(displayProps)
 			.timeout(timeout);
 	};
 
 	const destroy = id =>
 		knex.del().from(tableName).where({ id }).timeout(timeout);
 
+	const getAll = (displayProps = selectableProps) =>
+		knex.select(displayProps).from(tableName).timeout(timeout);
+
+	const getByID = (id, displayProps = selectableProps) =>
+		knex
+			.select(displayProps)
+			.from(tableName)
+			.where({ id })
+			.timeout(timeout);
+
+	const query = (filters, displayProps = selectableProps) =>
+		knex
+			.select(displayProps)
+			.from(tableName)
+			.where(filters)
+			.timeout(timeout);
+
+	// Same as `query` but only returns the first match if >1 are found.
+	const queryFirst = (filters, displayProps = selectableProps) =>
+		query(filters, displayProps).then(results => {
+			if (!Array.isArray(results)) return results;
+			return results[0];
+		});
+
+
+
+
+
 	return {
-		name,
-		tableName,
-		selectableProps,
-		timeout,
 		create,
-		findAll,
-		find,
-		findOne,
-		findById,
 		update,
-		destroy
+		destroy,
+		getAll,
+		getByID,
+		query,
+		queryFirst
 	};
 };
